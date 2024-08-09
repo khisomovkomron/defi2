71211c90ca2ad01c58934dc59cc4fbb0d6372ece
Komron Khisomov <normok_9595@bk.ru>
2024-08-09 18:53:36 +0500

    LAR token

--- src/LAR.sol
+++ src/LAR.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract LAR is ERC20 {
+
+    constructor() ERC20("LAR Token", "LAR") {
+        _mint(msg.sender, 100000 * 10 **decimals());
+    }
+}
