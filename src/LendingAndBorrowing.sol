d0cff34d579dc9bff1d57392643d097995aa8bb5
Komron Khisomov <normok_9595@bk.ru>
2024-08-09 18:56:26 +0500

    initiated LendingAndBorrowing

--- src/LendingAndBorrowing.sol
+++ src/LendingAndBorrowing.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import "./LendingHelper.sol";
+
+contract LendingAndBorrowing is Ownable{
+    using LendingHelper for address;
+
+
+    IERC20 public larToken;
+
+    constructor(address _token) {
+        larToken = IERC20(_token);
+    }
+}
