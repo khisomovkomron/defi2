ef6280b1527045532eea0f2d64002ef920741927
Komron Khisomov <normok_9595@bk.ru>
2024-08-09 18:52:35 +0500

    ADE token

--- src/ADE.sol
+++ src/ADE.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract ADE is ERC20 {
+
+    constructor() ERC20("ADE TOKEN", "ADE") {
+        _mint(msg.sender, 10000000 * 10**decimals());
+    }
+}
